0d75ff8527151eddfcb5ea74d681088807b1a486
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2012-06-22 07:12:18 +0200

    Modifying code2

---
+++ mywebapp/src/main/java/com/lotaris/labs/lpe/prototypes/mywebapp/services/Manager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lotaris.labs.lpe.prototypes.mywebapp.services;
+
+/**
+ *
+ * @author oliechti
+ */
+public class Manager {
+	
+}
