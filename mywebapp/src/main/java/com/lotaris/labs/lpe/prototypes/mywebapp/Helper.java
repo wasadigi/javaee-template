d74582c0f31bcdde9d7410cd9d9a59f3c3dc1c2f
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2012-06-22 06:31:38 +0200

    Adding project files

---
+++ mywebapp/src/main/java/com/lotaris/labs/lpe/prototypes/mywebapp/Helper.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lotaris.labs.lpe.prototypes.mywebapp;
+
+/**
+ *
+ * @author oliechti
+ */
+public class Helper {
+	
+}
